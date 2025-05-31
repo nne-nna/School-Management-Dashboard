2156c089e2c3f7f655fa8deacdd90ca4ffe97cfb
Ezidiegwu Nnenna <ezidiegwunnennafavour@gmail.com>
2025-05-31 13:51:37 +0100

    Added form modal

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useForm, SubmitHandler } from "react-hook-form"
+
+const TeacherForm = (
+    {
+        type,
+        data
+    }: 
+    {type:
+        "create" | 
+        "update"; 
+        data?:any
+    }) => {
+    return (
+        <form className="">
+         
+        </form>
+    )
+}
